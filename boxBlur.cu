850d1e15c7e41e9e7af5c138a8a3641aae8962da
rxmxndai <k.romann413@gmail.com>
2022-12-21 23:03:05 +0545

    lodepng decode image data

---
+++ boxBlur.cu
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lodepng.h"
+
+int main () {
+    // png decode variables
+    unsigned char *img;
+    unsigned int height, width;
+
+    // lodepng decode
+    lodepng_decode32_file(&img, &width, &height, "hck.png");
+
+    // image pixel's values RGBT and its space on memory
+    int totalValues = height * width * 4;
+    int totalSpace = totalValues * sizeof(unsigned char);
+
+    // dynamic memory allocation for image data in host
+    unsigned char *hostImageInput;
+    hostImageInput = (unsigned char *) malloc(totalSpace);
+    unsigned char *hostImageOutput;
+    hostImageOutput = (unsigned char *) malloc(totalSpace);
+
+
+    // PUT IMAGE DATA INSIDE host input image array
+    for (int i=0; i<totalValues; i++) {
+        hostImageInput[i]  = img[i];
+    }
+
+    // 
+
+
+
+
+
+
+
+    return 0;
+}
